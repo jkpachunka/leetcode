d3af609353eb03457923e79da3926905b832cd46
Jon Pachunka <jonpachunka21@gmail.com>
2021-11-15 23:33:22 -0600

    Skeleton code

---
+++ roman_to_int/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+map<char, int> numeral_map = {
+	{'I', 1},
+	{'V', 5},
+	{'X', 10},
+	{'L', 50},
+	{'C', 100},
+	{'D', 500},
+	{'M', 1000}
+};
+
+int roman_to_int(string input)
+{
+	vector<int> result_holder;
+	int result;
+	for (int i = 0; i < input.length(); i++)
+	{
+		
+	}
+	return 0;
+}
+
+int main() 
+{
+	return 0;
+}
